fe8c79160f7a46a8d574cd3945ce27f4d74c7617
unknown <ks.monish@hotmail.com>
2024-08-25 18:08:53 +0530

    add home page and card functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+  const loggedIn = { firstName: "Monish", lastName: 'KS' , email: "monishks2406@gmail.com" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and Manage you account transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2340}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+      user={loggedIn} 
+      trasactions={[]} 
+      banks={[{currentBalance: 1234.00},{currentBalance: 1500.00}]} />
+    </section>
+  );
+};
+
+export default Home;
