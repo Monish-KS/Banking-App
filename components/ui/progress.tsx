1cbbcec6bcdfcb27f9aad3dd4afd06a0cd6958fe
unknown <ks.monish@hotmail.com>
2024-09-02 21:40:47 +0530

    modify next config

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,13 +1,16 @@
-"use client"
-
+"use client";
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ProgressProps
 >(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -23,6 +26,7 @@ const Progress = React.forwardRef<
     />
   </ProgressPrimitive.Root>
 ))
+
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
