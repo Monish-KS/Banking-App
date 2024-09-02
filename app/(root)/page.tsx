8cbd9ca1625ea7607fde9814fa41441c0845344d
unknown <ks.monish@hotmail.com>
2024-09-02 21:27:55 +0530

    finalize the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -42,7 +42,7 @@ const Home = async ({searchParams: {id,page}}:SearchParamProps) => {
       </div>
       <RightSidebar 
       user={loggedIn} 
-      transactions={[account?.transactions]} 
+      transactions={account?.transactions} 
       banks={accountsData?.slice(0,2)} />
     </section>
   );
